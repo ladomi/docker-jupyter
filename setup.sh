b665a5d103987f84d0cdaf507d46dca19b4e3682
Norimasa Namioka <norimasa.namioka@g.softbank.co.jp>
2018-11-30 13:14:50 +0900

    add titanic data

--- setup.sh
+++ setup.sh
@@ -2,20 +2,22 @@
 
 script_name=${BASH_SOURCE}
 script_dir=$(cd $(dirname ${BASH_SOURCE}); pwd)
-
 echo ${script_name}
 echo ${script_dir}
-
 cd ${script_dir}
+
+################################################################################
 
 mkdir -p work
 
 # docker run -d --name jupyter -p 8888:8888 jupyter/datascience-notebook
 
-docker stop jupyter; docker rm jupyter
+docker stop jupyter
+docker rm jupyter
 docker run -d -v $(pwd)/work:/home/jovyan/work --name jupyter -p 8888:8888 jupyter/all-spark-notebook
 
 # docker exec jupyter /opt/conda/bin/pip install tensorflow
+sleep 5
 
 docker exec jupyter /opt/conda/bin/jupyter notebook list
 docker exec -it jupyter /bin/bash
--- start.sh
+++ start.sh
@@ -2,12 +2,13 @@
 
 script_name=${BASH_SOURCE}
 script_dir=$(cd $(dirname ${BASH_SOURCE}); pwd)
-
 echo ${script_name}
 echo ${script_dir}
-
 cd ${script_dir}
 
+################################################################################
+
 docker start jupyter
+sleep 5
 docker exec jupyter /opt/conda/bin/jupyter notebook list
 docker exec -it jupyter /bin/bash
